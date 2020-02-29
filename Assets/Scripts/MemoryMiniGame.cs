ec96cef7b7e522303ceda243efabe6f6a582b9a1
Nikhil Iyer <34634373+Nikhil-42@users.noreply,github.com>
2020-02-28 21:42:50 -0500

    timer tweak

--- Assets/Scripts/MemoryMiniGame.cs
+++ Assets/Scripts/MemoryMiniGame.cs
@@ -100,7 +100,7 @@ public class MemoryMiniGame : MiniGameBase
             yield return new WaitForSeconds(blinkTime);
 
             numbers[num].color = original;
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
