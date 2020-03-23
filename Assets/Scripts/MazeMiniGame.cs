fe8c120a67e713ed8c4e2aeb8e321466b67a816d
Nikhil Iyer <34634373+Nikhil-42@users.noreply,github.com>
2020-03-23 00:22:24 -0400

    maze game (without player render)

--- Assets/Scripts/MazeMiniGame.cs
+++ Assets/Scripts/MazeMiniGame.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class MazeMiniGame : MiniGameBase
 {
-    
-    public bool[,] grid;
+    public SpriteRenderer mazeRenderer;
 
-    public bool[] g;
-    public Vector2 spawnLocation;
-    public Vector2 finishLocation;
-
+    private int current;
     private int[] turtle;
     private int[] target;
 
     public override void MiniGameStart()
     {
-        grid = new bool[,] {
-            {true, true, true, false, true},
-            {true, false, true, false, true},
-            {true, false, true, false, true},
-            {true, false, true, false, true},
-            {true, false, true, true, true}
-        };
+        current = Random.Range(0, grids.GetLength(0));
 
-        turtle = new int[] {Mathf.RoundToInt(spawnLocation.x), Mathf.RoundToInt(spawnLocation.y)};
-        target = new int[] {Mathf.RoundToInt(finishLocation.x), Mathf.RoundToInt(finishLocation.y)};
+        mazeRenderer.sprite = mazes[current];
+        turtle = new int[] {Mathf.RoundToInt(spawnLocations[current].x), Mathf.RoundToInt(spawnLocations[current].y)};
+        target = new int[] {Mathf.RoundToInt(finishLocations[current].x), Mathf.RoundToInt(finishLocations[current].y)};
     }
 
     public override void SendInput(KeyCode key)
@@ -33,25 +24,25 @@ public class MazeMiniGame : MiniGameBase
         switch (key)
         {
             case KeyCode.DownArrow:
-                if (turtle[1] + 2 < 5 && grid[turtle[1] + 1, turtle[0]])
+                if (turtle[1] + 2 < 5 && grids[current, turtle[1] + 1, turtle[0]])
                 {
                     turtle[1] += 2;
                 }
                 break;
             case KeyCode.UpArrow:
-                if (turtle[1] - 2 >= 0 && grid[turtle[1] - 1, turtle[0]])
+                if (turtle[1] - 2 >= 0 && grids[current, turtle[1] - 1, turtle[0]])
                 {
                     turtle[1] -= 2;
                 }
                 break;
             case KeyCode.RightArrow:
-                if (turtle[0] + 2 < 5 && grid[turtle[1], turtle[0] + 1])
+                if (turtle[0] + 2 < 5 && grids[current, turtle[1], turtle[0] + 1])
                 {
                     turtle[0] += 2;
                 }
                 break;
             case KeyCode.LeftArrow:
-                if (turtle[0] - 2 >= 0 && grid[turtle[1], turtle[0] - 1])
+                if (turtle[0] - 2 >= 0 && grids[current, turtle[1], turtle[0] - 1])
                 {
                     turtle[0] -= 2;
                 }
@@ -69,4 +60,136 @@ public class MazeMiniGame : MiniGameBase
             print(Score);
         }
     }
+
+    
+    [SerializeField]
+    private Sprite[] mazes;
+    [SerializeField]
+    private Vector2[] spawnLocations;
+    [SerializeField]
+    private Vector2[] finishLocations;
+        
+    private bool[,,] grids = new bool[,,] 
+    {
+        { //#1
+            {true, true, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, true, true},
+        },
+        { //#2
+            {true, true, true, false, true},
+            {false, false, true, false, true},
+            {true, true, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, true, true},
+        },
+        { //#3
+            {true, true, true, true, true},
+            {true, false, false, false, false},
+            {true, true, true, true, true},
+            {false, false, true, false, true},
+            {true, true, true, false, true},
+        },
+        { //#4
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, true, true, false, true},
+            {true, false, false, false, true},
+            {true, true, true, true, true},
+        },
+        { //#5
+            {true, true, true, false, true},
+            {false, false, true, false, true},
+            {true, true, true, false, true},
+            {true, false, false, false, true},
+            {true, true, true, true, true},
+        },
+        { //#6
+            {true, false, true, true, true},
+            {true, false, true, false, false},
+            {true, false, true, true, true},
+            {true, false, false, false, true},
+            {true, true, true, true, true},
+        },
+        { //#7
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, true, true},
+            {true, false, false, false, true},
+            {true, true, true, true, true},
+        },
+        { //#8
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, true, true, true, true},
+        },
+        { //#9
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, true, true},
+            {true, false, true, false, false},
+            {true, true, true, false, true},
+        },
+        { //#10
+            {true, false, true, true, true},
+            {true, false, true, false, false},
+            {true, false, true, true, true},
+            {true, false, true, false, false},
+            {true, true, true, false, true},
+        },
+        { //#11
+            {true, true, true, false, true},
+            {false, false, true, false, true},
+            {true, true, true, false, true},
+            {false, false, true, false, true},
+            {true, false, true, true, true},
+        },
+        { //#12
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, true, true, false, true},
+            {false, false, true, false, true},
+            {true, false, true, true, true},
+        },
+        { //#13
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, true, true, false, true},
+            {false, false, true, false, true},
+            {true, false, true, true, true},
+        },
+        { //#14
+            {true, true, true, true, true},
+            {false, false, true, false, false},
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, true, true, false, true}
+        },
+        { //#15
+            {true, false, true, true, true},
+            {true, false, false, false, true},
+            {true, false, true, true, true},
+            {true, false, false, false, true},
+            {true, true, true, true, true}
+        },
+        { //#16
+            {true, true, true, true, true},
+            {false, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, false, true},
+            {true, true, true, false, true}
+        },
+        { //#17
+            {true, true, true, true, true},
+            {false, false, true, false, true},
+            {true, false, true, false, true},
+            {true, false, true, false, false},
+            {true, true, true, true, true}
+        },
+    };
+
 }
